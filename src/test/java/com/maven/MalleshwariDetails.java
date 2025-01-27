594e78629f4427bc7b6533f33e23eb5b1fc63bfe
rajanikumari180681 <rajanikumari180681@gmail.com>
2025-01-27 15:00:49 +0530

    added details

---
+++ src/test/java/com/maven/MalleshwariDetails.java
@@ -0,0 +1,16 @@
+package com.maven;
+
+public class MalleshwariDetails {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("This is malleshwari");
+		System.out.println("From Nizamabad");
+		System.out.println("Completed my schooling in Nalanda High School");
+		System.out.println("Intermeidiate in Vijay Sai, Bhodhan");
+		System.out.println("Graduated in Nishitha Degree College");
+		
+	}
+
+}
---
+++ src/test/java/com/maven/RajaniDetails.java
@@ -0,0 +1,12 @@
+package com.maven;
+
+public class RajaniDetails {
+
+	public static void main(String[] args) {
+		System.out.println("This is Rajani");
+		System.out.println("From Hyderabad");
+		System.out.println("my hobbies are Cooking ,learning new technologies");
+
+	}
+
+}
